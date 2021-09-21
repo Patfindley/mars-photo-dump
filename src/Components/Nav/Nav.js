ff279216e6272aa51f73d16afd39d3742fce35db
Patrick <patfindley@gmail.com>
2021-09-21 16:10:03 -0400

    Add burger for mobile

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,19 +4,31 @@ import './Nav.css'
 import marsIcon from '../../assets/mars_icon.png'
 
 const Nav = () => {
+  const [navActive, setNavActive] = useState(false)
+
   return (
     <div className='nav-container'>
-      <img className='nav-icon' src={marsIcon} />
+      <div
+				className={navActive ? "burger-container open" : "burger-container"}
+				onClick={() => setNavActive(!navActive)}>
+				<span className="patty top"></span>
+				<span className="patty mid"></span>
+				<span className="patty bottom"></span>
+			</div>
       <h2 className='nav-title'>Mars Eye Candy</h2>
-      <ul className='nav-links'>
-        <NavLink
-          exact={true}
-          to='/'
-          activeClassName='nav-selected'>
-            <li>HOME</li>
-          </NavLink>
-          <li>LIKED</li>
-      </ul>
+      <img className='nav-icon' src={marsIcon} />
+      <div className={navActive ? "nav-open" : "hidden"}>
+        <ul className='nav-links'>
+          <NavLink
+            exact={true}
+            to='/'
+            activeClassName='nav-selected'>
+              <li>HOME</li>
+            </NavLink>
+            <li>LIKED</li>
+            <li>ABOUT</li>
+        </ul>
+      </div>
     </div>
   )
 }
