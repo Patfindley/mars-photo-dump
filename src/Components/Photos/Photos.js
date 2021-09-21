63930ba3a9d0c84b313ece8d84751d234a1bbba5
Patrick <patfindley@gmail.com>
2021-09-21 15:06:27 -0400

    Heart ohver effect

--- src/Components/Photos/Photos.js
+++ src/Components/Photos/Photos.js
@@ -14,7 +14,8 @@ const Photos = ({ marsPhotos, error }) => {
             <img className='heart-fill' src={heartFill} />
             <img className='photo photo-${index}' src={photo.img_src} alt={`${photo.rover.name} ${photo.camera.full_name}`}/>
             <div className='photo-details' >
-              <h3 className='rover-name'>taken by: {photo.rover.name}</h3>
+              <h5 className='taken-on'>taken: {photo.earth_date}</h5>
+              {/* <h3 className='rover-name'>taken by: {photo.rover.name}</h3> */}
             </div>
           </div>
         )
