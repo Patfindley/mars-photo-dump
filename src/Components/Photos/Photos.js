3640296ba27925a199b58a30bc3105b3b5069389
Patrick <patfindley@gmail.com>
2021-09-21 14:48:42 -0400

    Add heart icon

--- src/Components/Photos/Photos.js
+++ src/Components/Photos/Photos.js
@@ -1,4 +1,6 @@
 import './Photos.css'
+import heartBorder from '../../assets/heart_border_icon.svg'
+import heartFill from '../../assets/heart_fill_icon.svg'
 
 const Photos = ({ marsPhotos, error }) => {
   console.log(marsPhotos, 'photos.js')
@@ -8,6 +10,8 @@ const Photos = ({ marsPhotos, error }) => {
       return marsPhotos.map((photo, index) => {
         return (
           <div className={`photo-wrap`} key={photo.id}>
+            <img className='heart-border' src={heartBorder} />
+            <img className='heart-fill' src={heartFill} />
             <img className='photo photo-${index}' src={photo.img_src} alt={`${photo.rover.name} ${photo.camera.full_name}`}/>
             <div className='photo-details' >
               <h3 className='rover-name'>taken by: {photo.rover.name}</h3>
