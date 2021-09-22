b75988ad3419f54179e5d56f12bd466da3d2e782
Patrick <patfindley@gmail.com>
2021-09-21 23:20:47 -0400

    Update util api key

--- src/Util.js
+++ src/Util.js
@@ -1,9 +1,11 @@
+const API_Key = process.env.REACT_APP_API_KEY
+
 export const getDateString = (date) => {
   return `${date.getFullYear()}-0${date.getMonth() + 1}-${date.getDate()}`
 }
 
 export const getPhotosForDay = (date) => {
-  return fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${getDateString(date)}&api_key=${process.env.REACT_APP_API_KEY}`)
+  return fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${getDateString(date)}&api_key=${API_Key}`)
 }
 
 export const getPreviousDate = (date, diff) => {
